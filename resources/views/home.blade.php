d132846b7d8ee8774da9a9b66dbe6c80bc37c5d1
Dang Nguyen <dang.nguyen@ptnglobalcorp.com>
2023-01-10 13:54:58 +0700

    Create homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+
+        <!-- Styles -->
+        <style>
+
+        </style>
+    </head>
+    <body class="p-5">
+        @include('common.header')
+        
+        @include('common.footer')
+    </body>
+</html>
